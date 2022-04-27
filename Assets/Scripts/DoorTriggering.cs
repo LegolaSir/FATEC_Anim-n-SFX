354827fdb9fb619e5b419cc3f1323898a3f6db50
Bruno Gonçalves de Alencar <brunoalencar2002@hotmail.com>
2022-04-27 15:51:47 -0300

    Preparing Scene

---
+++ Assets/Scripts/DoorTriggering.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriggering : MonoBehaviour
+{
+    /* Attributes */
+    private Animator anim;
+
+    /* Unity Methods */
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            anim.SetBool("IsNear", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            anim.SetBool("IsNear", false);
+        }
+    }
+}
